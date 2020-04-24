e97a940f098048c07818f2636558249843156694
Remak3r <64277445+Remak3r@users.noreply.github.com>
2020-04-24 18:28:49 +0100

    Adicionei arquivos via upload

---
+++ Hello Github/src/br/com/joseassis/git/HelloGit.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package br.com.joseassis.git;
+
+/**
+ * @author Andre
+ * Exemplo de integracao do eclipse com github
+ */
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+System.out.println("Hello Git");
+	}
+
+}
